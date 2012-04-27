917ecb7654593b30331fe630b2f993ce6bbe731f
Matthew Brick <mbrick1000@gmail.com>
2012-04-26 20:36:51 -0700

    test LCD attempt 1

---
+++ robolock/src/LCD.c
@@ -0,0 +1,48 @@
+/** RoboLock
+ * LED.C
+ *
+ * Source file for GPIO control, configuration and usage of LEDs on the RoboLock board
+ *
+ * v1.0 - 4/22/2012
+ */
+
+#include "LPC23xx.h"
+#include "LCD.h"
+#include "type.h"
+#include "target.h"
+
+volatile BYTE I2CMasterBuffer[BUFSIZE];
+ BYTE buff;
+
+void initLCD(void) {
+I2CInit(0);
+
+}
+
+void clearLCD(void) {
+	buff = 0x01;
+	I2CMasterBuffer[0] = buff;
+	I2C1MasterHandler();
+}
+
+
+void printLCD(BYTE val) {
+	buff = val;
+	I2CMasterBuffer[0] =buff;
+	I2C1MasterHandler();
+}
+
+
+void testLCD() {
+		int i, j;
+		j = 0;
+		i = 0;
+		clearLCD();
+
+		for (i = 0; i < 10; i++) {
+		printLCD(0x42);
+		delay();
+		printLCD(0x43);
+		delay();
+		}
+}
