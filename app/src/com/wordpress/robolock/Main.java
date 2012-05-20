d9f444237573c37ac0482cbbf7b974d74114bf40
Brian Dunlay <brian.dunlay@gmail.com>
2012-05-20 04:02:25 -0700

    adding android project

---
+++ app/src/com/wordpress/robolock/Main.java
@@ -0,0 +1,79 @@
+package com.wordpress.robolock;
+
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+
+public class Main extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		function();
+	}
+
+	
+	static Bitmap downloadBitmap(String url) {
+	    final DefaultHttpClient client = new DefaultHttpClient();
+	    final HttpGet getRequest = new HttpGet(url);
+
+	    try {
+	        HttpResponse response = client.execute(getRequest);
+	        final int statusCode = response.getStatusLine().getStatusCode();
+	        if (statusCode != HttpStatus.SC_OK) { 
+	            Log.w("ImageDownloader", "Error " + statusCode + " while retrieving bitmap from " + url); 
+	            return null;
+	        }
+	        
+	        final HttpEntity entity = response.getEntity();
+	        if (entity != null) {
+	            InputStream inputStream = null;
+	            try {
+	                inputStream = entity.getContent(); 
+	                final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+	                return bitmap;
+	            } finally {
+	                if (inputStream != null) {
+	                    inputStream.close();  
+	                }
+	                entity.consumeContent();
+	            }
+	        }
+	    } catch (Exception e) {
+	        // Could provide a more explicit error message for IOException or IllegalStateException
+	        getRequest.abort();
+	        Log.v("ImageDownloader", "Error while retrieving bitmap from " + url + e.toString());
+	    } finally {
+	    }
+	    return null;
+	}
+	
+	void function() {
+		
+
+		Bitmap image = downloadBitmap("http://192.168.0.1/image.png");
+
+		ImageView imview = (ImageView) findViewById(R.id.photo);
+		
+		imview.setImageBitmap(image);
+	}
+
+	
+	
+	
+	
+}
